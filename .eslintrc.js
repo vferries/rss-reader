44cbcb89b47387ded61806e9836d94db406a6d0c
Vincent FERRIES <vincent.ferries@gmail.com>
2022-06-02 18:34:40 +0200

    :hammer: Add eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "@solid/eslint-config-base",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/naming-convention": "off",
+    "import/no-default-export": "off",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "src/"],
+      },
+    },
+  },
+  ignorePatterns: ".eslintrc.js",
+};
