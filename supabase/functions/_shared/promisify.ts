e8e22b74a27cec2e113945f429397a05eedd100a
Vincent FERRIES <vincent.ferries@gmail.com>
2022-07-20 18:28:46 +0200

    :sparkles: Backend replaced by a Supabase Function

---
+++ supabase/functions/_shared/promisify.ts
@@ -0,0 +1,109 @@
+export type AsyncFunction0<O1, O2, O3, O4, O5, O6> = {
+  (callback?: (o1: O1, o2: O2, o3: O3, o4: O4, o5: O5, o6: O6) => void): void;
+};
+
+export type AsyncFunction1<I, O1, O2, O3, O4, O5, O6> = {
+  (
+    i: I,
+    callback?: (o1: O1, o2: O2, o3: O3, o4: O4, o5: O5, o6: O6) => void,
+  ): void;
+};
+
+export type AsyncFunction2<I1, I2, O1, O2, O3, O4, O5, O6> = {
+  (
+    i1: I1,
+    i2: I2,
+    callback?: (o1: O1, o2: O2, o3: O3, o4: O4, o5: O5, o6: O6) => void,
+  ): void;
+};
+
+export type AsyncFunction3<I1, I2, I3, O1, O2, O3, O4, O5, O6> = {
+  (
+    i1: I1,
+    i2: I2,
+    i3: I3,
+    callback?: (o1: O1, o2: O2, o3: O3, o4: O4, o5: O5, o6: O6) => void,
+  ): void;
+};
+
+export type AsyncFunction4<I1, I2, I3, I4, O1, O2, O3, O4, O5, O6> = {
+  (
+    i1: I1,
+    i2: I2,
+    i3: I3,
+    i4: I4,
+    callback?: (o1: O1, o2: O2, o3: O3, o4: O4, o5: O5, o6: O6) => void,
+  ): void;
+};
+
+export function typed<O1, O2, O3, O4, O5, O6>(
+  af: AsyncFunction0<O1, O2, O3, O4, O5, O6>,
+): () => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed<O1, O2, O3, O4, O5, O6>(
+  c: Object,
+  af: AsyncFunction0<O1, O2, O3, O4, O5, O6>,
+): () => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed<I, O1, O2, O3, O4, O5, O6>(
+  af: AsyncFunction1<I, O1, O2, O3, O4, O5, O6>,
+): (i: I) => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed<I, O1, O2, O3, O4, O5, O6>(
+  c: Object,
+  af: AsyncFunction1<I, O1, O2, O3, O4, O5, O6>,
+): (i: I) => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed<I1, I2, O1, O2, O3, O4, O5, O6>(
+  af: AsyncFunction2<I1, I2, O1, O2, O3, O4, O5, O6>,
+): (i1: I1, i2: I2) => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed<I1, I2, O1, O2, O3, O4, O5, O6>(
+  c: Object,
+  af: AsyncFunction2<I1, I2, O1, O2, O3, O4, O5, O6>,
+): (i1: I1, i2: I2) => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed<I1, I2, I3, O1, O2, O3, O4, O5, O6>(
+  af: AsyncFunction3<I1, I2, I3, O1, O2, O3, O4, O5, O6>,
+): (i1: I1, i2: I2, i3: I3) => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed<I1, I2, I3, O1, O2, O3, O4, O5, O6>(
+  c: Object,
+  af: AsyncFunction3<I1, I2, I3, O1, O2, O3, O4, O5, O6>,
+): (i1: I1, i2: I2, i3: I3) => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed<I1, I2, I3, I4, O1, O2, O3, O4, O5, O6>(
+  af: AsyncFunction4<I1, I2, I3, I4, O1, O2, O3, O4, O5, O6>,
+): (i1: I1, i2: I2, i3: I3, i4: I4) => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed<I1, I2, I3, I4, O1, O2, O3, O4, O5, O6>(
+  c: Object,
+  af: AsyncFunction4<I1, I2, I3, I4, O1, O2, O3, O4, O5, O6>,
+): (i1: I1, i2: I2, i3: I3, i4: I4) => Promise<[O1, O2, O3, O4, O5, O6]>;
+export function typed(...inputs: any[]): any {
+  return __generic__(inputs);
+}
+
+export function generic(
+  context: Object,
+  asyncFunction: Function,
+): (...inputs: any[]) => Promise<any[]>;
+export function generic(
+  asyncFunction: Function,
+): (...inputs: any[]) => Promise<any[]>;
+export function generic(...inputs: any[]): Function {
+  return __generic__(inputs);
+}
+
+function __generic__(inputs: any[]): Function {
+  let context: Object;
+  let asyncFunction: Function;
+
+  if (inputs.length == 2) {
+    context = inputs[0];
+    asyncFunction = inputs[1];
+  } else {
+    context = {};
+    asyncFunction = inputs[0];
+  }
+
+  return (...inputs: any[]) => {
+    return new Promise<any[]>((resolve) => {
+      asyncFunction.apply(
+        context,
+        inputs.concat([(...outputs: any[]) => resolve(outputs)]),
+      );
+    });
+  };
+}
--- supabase/functions/add-feed/index.ts
+++ supabase/functions/add-feed/index.ts
@@ -1,12 +1,98 @@
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
+import xml from "https://esm.sh/xml2js@^0.4.23";
+import { Article } from "../_shared/model/article.ts";
+import { Feed } from "../_shared/model/feed.ts";
+import { client } from "../_shared/supabaseClient.ts";
 
-console.log("Hello from Functions!");
+type TextContent = { _: string } | string;
+
+type AtomEntry = {
+  title: TextContent;
+  link: { $: { href: string } };
+  summary: TextContent;
+  content: TextContent;
+  published: TextContent;
+};
+type RSSEntry = {
+  title: TextContent;
+  link: TextContent;
+  description: TextContent;
+  pubDate: TextContent;
+};
+type AtomFeed = { feed: { entry: AtomEntry[]; title: TextContent } };
+type RSSFeed = { rss: { channel: { item: RSSEntry[]; title: TextContent } } };
+
+async function upsertFeed(feed: Feed) {
+  let { error } = await client.from<Feed>("feed").upsert(feed);
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("Feed pushed to Supabase");
+  }
+}
+
+async function upsertArticles(articles: Article[]) {
+  let { error } = await client.from<Article>("article").upsert(articles);
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("Articles pushed to Supabase");
+  }
+}
+
+const parser = new xml.Parser({
+  explicitArray: false,
+});
+
+function getTextContent(text: TextContent) {
+  return typeof text === "object" ? text._ : text;
+}
+
+const atomToArticle = (entry: AtomEntry): Article => {
+  return {
+    title: getTextContent(entry.title),
+    link: entry.link.$.href,
+    read: false,
+    description: getTextContent(entry.summary) || getTextContent(entry.content),
+    published: getTextContent(entry.published),
+  };
+};
+
+const rssToArticle = (entry: RSSEntry): Article => {
+  return {
+    title: getTextContent(entry.title),
+    link: getTextContent(entry.link),
+    read: false,
+    description: getTextContent(entry.description),
+    published: getTextContent(entry.pubDate),
+  };
+};
+
+const parseFeed = async (url: string): Promise<Feed> => {
+  const result = await fetch(url);
+  const text = await result.text();
+  const feed = { url, title: "" };
+
+  if (result.ok) {
+    const doc: AtomFeed | RSSFeed = await parser.parseStringPromise(text);
+    const articles = "rss" in doc
+      ? doc.rss.channel.item.map(rssToArticle)
+      : doc.feed.entry.map(atomToArticle);
+    feed.title = getTextContent(
+      "rss" in doc ? doc.rss.channel.title : doc.feed.title,
+    );
+
+    await upsertFeed(feed);
+    await upsertArticles(articles);
+  } else {
+    console.error(result.statusText, text);
+  }
+  return feed;
+};
 
 serve(async (req) => {
-  const { name } = await req.json();
-  const data = {
-    message: `Hello ${name}!`,
-  };
+  const { url } = await req.json();
+  const data = await parseFeed(url);
 
   return new Response(JSON.stringify(data), {
     headers: { "Content-Type": "application/json" },
@@ -15,8 +101,5 @@ serve(async (req) => {
 
 /*
  * To invoke:
- * curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
- *   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' \
- *   --header 'Content-Type: application/json' \
- *   --data '{"name":"Functions"}'
+ * curl -i --location --request POST 'http://localhost:54321/functions/v1/' --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs' --header 'Content-Type: application/json' --data '{"url":"https://www.enveille.info/feed.xml"}'
  */
