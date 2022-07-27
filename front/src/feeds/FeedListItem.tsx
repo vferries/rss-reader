36261c3310f0b22533e8310840e4751723a9ad78
Vincent FERRIES <vincent.ferries@gmail.com>
2022-07-27 19:19:38 +0200

    :sparkles: Add feed refresh button

--- front/src/feeds/FeedListItem.tsx
+++ front/src/feeds/FeedListItem.tsx
@@ -1,12 +1,20 @@
 import type { Feed } from "shared/model/feed";
 import styles from "../articles/ArticleList.module.css";
+import { client } from "../utils/supabaseClient";
 
 export function FeedListItem({ feed }: { feed: Feed }) {
+  const refreshFeed = () => {
+    const body = JSON.stringify({ url: feed.url });
+    void client.functions.invoke("refresh-feed", { body });
+  };
   return (
     <>
       <a href={feed.url} class={`${styles.title} ${styles.cell}`}>
         {feed.title}
       </a>
+      <button type="button" onClick={refreshFeed}>
+        Refresh feed
+      </button>
     </>
   );
 }
